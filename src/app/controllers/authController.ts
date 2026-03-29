5c2fc1d8e5f8f1eec0eeac766ce195a5649fa5f9
Xcode Hanji <truongcanhchan@gmail.com>
2026-03-29 16:54:41 +0700

    Feat: user

--- src/app/controllers/authController.ts
+++ src/app/controllers/authController.ts
@@ -1,10 +1,45 @@
 const UserSchema = require("../models/User");
+import { Router, Response } from 'express';
+
+function error(res : Response, statusCode : number, message : string, At : string) {
+    res.status(statusCode);
+    res.send({
+        statusCode : statusCode,
+        message : message,
+        At : At
+    });
+}
 
 class authController {
     // [GET]
     async index(req, res) {
-        const user = await UserSchema.find({});
-        res.send(user);
+        const isValidEmail = (e) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e);
+        const Email : string = req.query.email;
+        const Password : string = req.query.password;
+        
+        if (!Email.trim()) {
+            error(res, 400, "Email is required", "Email");
+            return;
+        } else if (!isValidEmail(Email)) {
+            error(res, 400, "Please enter a valid email address", "Email");
+            return;
+        }
+
+        if (!Password) {
+            error(res, 400, "Password is required", "Password");
+            return;
+        } else if (Password.length < 6) {
+            error(res, 400, "Password must be at least 6 characters", "Password");
+            return;
+        }
+
+        const user = await UserSchema.findOne({"email" : Email, "password" : Password});
+        if (user == null) {
+            error(res, 400, "Incorrect Email or Password","Password");
+            return;
+        }
+
+        return res.send("Hello world");
     }
 };
 
